9ec3e086beab6ce0651b995c9a62d26742bce349
SC2 <hectorjesusm08@gmail.com>
2025-03-19 15:55:06 -0600

    examen

---
+++ Clases_Y_objetos/Relaciones_entre_objetos.py
@@ -0,0 +1,60 @@
+
+"""11 de marzo.
+    Hector Jesus Mendez Santiago
+    Programacion orientada a objetos: Programa que crea objetos
+    Se relaciona con las clases.
+    relación uno a muchos, relacion de agregacion
+
+"""
+
+
+class Empleado:
+    no_id = 1
+    def __init__(self, nombre:str, sueldo:float ):
+        self.nombre= nombre
+        self.sueldo= sueldo
+        self.id = Empleado.no_id
+        Empleado.no_id += 1
+    def aumentar_sueldo (self, porcentaje:float)->None:
+        pass
+
+    def __str__(self)->str:
+        return f"Empleado: {self.nombre}, id: {self.id}, sueldo:{self.sueldo}"
+
+class Empresa:
+    def __init__(self, nombre:str, *empleados:Empleado)->None:
+        self.nombre =nombre
+        self.empleados = list(empleados)
+    def agregar_empleados (self, *nuevos_empleados:Empleado):
+        for empleado in list(nuevos_empleados):
+            self.empleados.append(empleado)
+
+    def __str__(self) -> str:
+        """
+        Se utiliza para mostrar los empleados de la empresa en forma de lista.
+        """
+        # Se convierte los elementos de la lista en cadenas (invocando str() para cada uno de ellos) y
+        # se unen con ", " a través del métod0 str.join().
+        # Este patrón es muy común en Python para obtener una cadena a partir de una lista.
+        empleados = ", ".join(str(empleado) for empleado in self.empleados)
+        return f"Empresa({self.nombre = }, {empleados})"
+
+    def mostrar_empleados(self) -> None:
+        """
+        Se utiliza para mostrar los empleados de la empresa en forma de lista.
+        """
+        print(f"*** Lista de empleados de {self.nombre} ***")
+
+        for empleado in self.empleados:
+            print(empleado)
+
+
+if __name__ == '__main__':
+    alberto = Empleado("alberto", 125.50)
+    print(alberto)
+    addi = Empleado("Adrian", 345.50)
+    print(addi)
+
+    unsij = Empresa("Unsij", alberto, addi)
+    unsij.mostrar_empleados()
+    print(unsij)
--- Codewars/KeepHydrated_8KYU.py
+++ Codewars/KeepHydrated_8KYU.py
@@ -1,5 +1,6 @@
-""""
-Descripcion: A Nathan le encanta el ciclismo.
+"""
+Nombre: Héctor Jesús Méndez Santiago
+Descripción: A Nathan le encanta el ciclismo.
 
 Debido a que Nathan sabe que es importante mantenerse hidratado, bebe 0.5 litros de agua por hora de ciclismo.
 
@@ -13,35 +14,17 @@ time = 6.7---> litres = 3
 
 time = 11.8--> litres = 5
 """
-def cadena_a_flotante(cadena: str) -> float | None:
+def operaciones (tiempo_recorrido)->int:
     """
-    Función que convierte una cadena a un número flotante.
-    :param cadena: Cadena a convertir.
-    :return: Regresa el número flotante si cumple con el formato, en caso contrario, devuelve None.
+    Función que realiza operaciones para encontrar la cantidad de litros que se toman
+    en un determinado tiempo.
+    :param tiempo_recorrido: Tiempo de recorrido, es dado por el usuario.
+    :return: número entero para que sea redondeado.
     """
-    no_puntos = cadena.count(".")
-    no_guiones = cadena.count("-")
-    revisar_cadena = cadena.lstrip('-').replace(".", "")
-
-    if revisar_cadena.isnumeric() and no_guiones in (0, 1) and no_puntos in (0, 1):
-        return float(cadena)
-    else:
-        return None
-
-
-
-
-
-
-def menu_principal ():
-    print("\nEste es el programa que calcula cuantos litros de agua se toma en un determinado tiempo")
-    tiempo = input("Ingresa cuantas horas se ha recorrido: ")
-    while not cadena_a_flotante(tiempo):
-        tiempo = input("Error, intenta denuevo: ")
-
-
-
-    print(f"Los litros tomados son {tiempo * 0.5}")
-
+    resultado = tiempo_recorrido * 0.5
+    return int(resultado)
 if __name__ == '__main__':
-    menu_principal()
+    print(operaciones(1))
+    print(operaciones(6.7))
+    print(operaciones(11.8))
+    print(operaciones(4))
